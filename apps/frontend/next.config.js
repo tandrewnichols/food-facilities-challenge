d7e5036fb6199b664d1e35fe3e2a1b2bdf3dc2fe
Andrew Nichols <tandrewnichols@gmail.com>
2024-03-04 05:20:59 -0500

    Docker

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,27 +1,15 @@
-//@ts-check
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require('@nx/next');
-
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
-const nextConfig = {
-  nx: {
-    // Set this to true if you would like to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
-  },
+module.exports = {
+  reactStrictMode: true,
   transpilePackages:  [
     '@components',
     '@hooks',
     '@api'
-  ]
+  ],
+  output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
-
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
-
-module.exports = composePlugins(...plugins)(nextConfig);
